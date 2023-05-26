700a7608dd4e44d5906568d07ec931e8ccd0d08b
Kletkin Vasilii <kletkinvasilii@gmail.com>
2023-05-26 19:57:55 +0400

    fix

--- django_search/tickets/admin.py
+++ django_search/tickets/admin.py
@@ -5,7 +5,7 @@ from .models import Ticket
 class TicketAdmin(admin.ModelAdmin):
     list_display = ('title', 'is_active', )
     fields = ("title", "url", "telegram_username", "is_active", "telegram", "created_by")
-    readonly_fields = ("telegram",)
+    readonly_fields = ("telegram", "created_by")
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
