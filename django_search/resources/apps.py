e3b0e29a7e8d2b99580e784e23483b146ebe9d2c
Kletkin Vasilii <kletkinvasilii@gmail.com>
2023-07-27 12:36:12 +0400

    add fix

--- django_search/resources/apps.py
+++ django_search/resources/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class ResourcesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'resources'
-    
-    def ready(self):
-        from . import signals
-        from . import tasks
--- django_search/resources/tasks.py
+++
@@ -1,7 +0,0 @@
-
-from .models import Resource
-
-def parse_resources():
-    resources = Resource.objects.all()
-    for resource in resources:
-        resource.parse()
