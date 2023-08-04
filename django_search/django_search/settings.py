efdc6affadc3d8ca346543f0ba6bab9c5ad55464
Kletkin Vasilii <kletkinvasilii@gmail.com>
2023-08-04 20:49:28 +0400

    add fix

--- django_search/django_search/settings.py
+++ django_search/django_search/settings.py
@@ -113,15 +113,8 @@ DATABASES = {
     }
 }
 
-RABBITMQ = {
-    "PROTOCOL": os.environ.get("PROTOCOL", "amqp"),
-    "HOST": os.environ.get("RABBITMQ_HOST", "localhost"),
-    "PORT": os.environ.get("RABBITMQ_PORT", 5672),
-    "USER": os.environ.get("RABBITMQ_DEFAULT_USER", "guest"),
-    "PASSWORD": os.environ.get("RABBITMQ_DEFAULT_PASS", "guest"),
-}
-
-CELERY_BROKER_URL = f"{RABBITMQ['PROTOCOL']}://{RABBITMQ['USER']}:{RABBITMQ['PASSWORD']}@{RABBITMQ['HOST']}:{RABBITMQ['PORT']}"
+CELERY_BROKER_URL = os.environ.get("BROKER_URL", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("RESULT_BACKEND", "redis://localhost:6379/0")
 
 
 # Password validation
