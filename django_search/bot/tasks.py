2370ba7c16053779ca8cc015703b48494585f1c3
Kletkin Vasilii <kletkinvasilii@gmail.com>
2023-08-10 23:32:09 +0400

    add fix

--- django_search/bot/tasks.py
+++ django_search/bot/tasks.py
@@ -9,7 +9,7 @@ def send_ads():
     ads = Ad.objects.filter(is_sent=False).select_related("ticket", "ticket__telegram")
     
     for ad in ads:
-        message = f"New ad : {ad.ticket.title} {ad.url}"
+        message = f"{ad.ticket.title}:{ad.title} {ad.url}"
         user_id = ad.ticket.telegram.user_id
         bot.send_message(user_id, message)
     ads.update(is_sent=True)
